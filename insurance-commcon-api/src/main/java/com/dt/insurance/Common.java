571d57b89e7205482c642993200f89b2df09f3b3
wb_glb <weibin9966@qq.com>
2020-06-23 21:24:22 +0800

    first

---
+++ insurance-commcon-api/src/main/java/com/dt/insurance/Common.java
@@ -0,0 +1,7 @@
+package com.dt.insurance;
+
+/**
+ * Create By WeiBin on 2020/6/23 21:14
+ */
+public class Common {
+}
---
+++ insurance-user/src/main/java/com/dt/insurance/UserSpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.dt.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Create By WeiBin on 2020/6/23 21:17
+ * @author WB
+ */
+@SpringBootApplication
+public class UserSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserSpringBootApplication.class,args);
+    }
+}
