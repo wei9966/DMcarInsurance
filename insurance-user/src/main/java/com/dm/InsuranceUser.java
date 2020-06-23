352858cb957b1f35bfc713172e2058a83ad9dd47
wd9746 <wd974655958@163.com>
2020-06-23 21:17:33 +0800

    345

---
+++ insurance-user/src/main/java/com/dm/InsuranceUser.java
@@ -0,0 +1,18 @@
+package com.dm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ProjectName: insurance
+ * @ClassName: InsuranceUser
+ * @Description: TODO(一句话描述该类的功能)
+ * @Author: WD
+ * @Date: 2020/6/23 21:09
+ */
+@SpringBootApplication
+public class InsuranceUser {
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceUser.class, args);
+    }
+}
