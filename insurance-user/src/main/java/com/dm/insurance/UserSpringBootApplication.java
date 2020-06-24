f8e04b5e947632824cebf4bf101c09c1a41f2d39
wb_glb <weibin9966@qq.com>
2020-06-24 20:29:13 +0800

    网关整合用户服务

--- insurance-user/src/main/java/com/dm/insurance/UserSpringBootApplication.java
+++ insurance-user/src/main/java/com/dm/insurance/UserSpringBootApplication.java
@@ -2,13 +2,14 @@ package com.dm.insurance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Create By WeiBin on 2020/6/23 21:17
  * @author WB
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
 public class UserSpringBootApplication {
     public static void main(String[] args) {
