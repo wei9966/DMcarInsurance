d58f7178a61e00bb9faad6c2147d4d784f9e6e0b
zhulinyan <1749353090@qq.com>
2020-06-27 08:33:35 +0800

    zly6/27 01

---
+++ insurance-other-insur-info/src/main/java/com/dm/insurance/entity/InsuranceOtherInsuranceInformation.java
@@ -0,0 +1,88 @@
+package com.dm.insurance.entity;
+
+import java.util.Date;
+import java.io.Serializable;
+
+/**
+ * (InsuranceOtherInsuranceInformation)实体类
+ *
+ * @author makejava
+ * @since 2020-06-24 17:26:27
+ */
+public class InsuranceOtherInsuranceInformation implements Serializable {
+    private static final long serialVersionUID = 285606446520915538L;
+    /**
+    * 其他投保信息id
+    */
+    private Integer otherInsuranceInformationId;
+    /**
+    * 其他投保信息起保日期
+    */
+    private Date otherInsuranceInformationDateBegin;
+    /**
+    * 其他投保信息结束日期
+    */
+    private Date otherInsuranceInformationDateFinish;
+    /**
+    * 其他投保信息姓名
+    */
+    private String otherInsuranceInformationName;
+    /**
+    * 其他投保信息身份证
+    */
+    private String otherInsuranceInformationCard;
+    /**
+    * 其他投保信息邮箱
+    */
+    private String otherInsuranceInformationEmail;
+
+
+    public Integer getOtherInsuranceInformationId() {
+        return otherInsuranceInformationId;
+    }
+
+    public void setOtherInsuranceInformationId(Integer otherInsuranceInformationId) {
+        this.otherInsuranceInformationId = otherInsuranceInformationId;
+    }
+
+    public Date getOtherInsuranceInformationDateBegin() {
+        return otherInsuranceInformationDateBegin;
+    }
+
+    public void setOtherInsuranceInformationDateBegin(Date otherInsuranceInformationDateBegin) {
+        this.otherInsuranceInformationDateBegin = otherInsuranceInformationDateBegin;
+    }
+
+    public Date getOtherInsuranceInformationDateFinish() {
+        return otherInsuranceInformationDateFinish;
+    }
+
+    public void setOtherInsuranceInformationDateFinish(Date otherInsuranceInformationDateFinish) {
+        this.otherInsuranceInformationDateFinish = otherInsuranceInformationDateFinish;
+    }
+
+    public String getOtherInsuranceInformationName() {
+        return otherInsuranceInformationName;
+    }
+
+    public void setOtherInsuranceInformationName(String otherInsuranceInformationName) {
+        this.otherInsuranceInformationName = otherInsuranceInformationName;
+    }
+
+    public String getOtherInsuranceInformationCard() {
+        return otherInsuranceInformationCard;
+    }
+
+    public void setOtherInsuranceInformationCard(String otherInsuranceInformationCard) {
+        this.otherInsuranceInformationCard = otherInsuranceInformationCard;
+    }
+
+    public String getOtherInsuranceInformationEmail() {
+        return otherInsuranceInformationEmail;
+    }
+
+    public void setOtherInsuranceInformationEmail(String otherInsuranceInformationEmail) {
+        this.otherInsuranceInformationEmail = otherInsuranceInformationEmail;
+    }
+
+}
--- insurance-type/src/main/java/com/dm/insurance/dao/InsuranceCarInsurDao.java
+++ insurance-type/src/main/java/com/dm/insurance/dao/InsuranceCarInsurDao.java
@@ -1,6 +1,7 @@
 package com.dm.insurance.dao;
 
 import com.dm.insurance.entity.InsuranceCarInsur;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
@@ -10,6 +11,7 @@ import java.util.List;
  * @author makejava
  * @since 2020-06-24 14:40:21
  */
+@Mapper
 public interface InsuranceCarInsurDao {
 
     /**
