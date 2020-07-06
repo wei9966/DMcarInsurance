282026ef9a1aeb7f82f5e5d68c787f275773783b
dmt <1148098878@qq.com>
2020-07-06 11:20:50 +0800

    大木头  整合redis模块

--- insurance-city/src/main/java/com/dm/insurance/controller/InsuranceCarInfoController.java
+++ insurance-city/src/main/java/com/dm/insurance/controller/InsuranceCarInfoController.java
@@ -1,11 +1,17 @@
 package com.dm.insurance.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.dm.insurance.entity.InsuranceCarInfo;
 import com.dm.insurance.entity.R;
 import com.dm.insurance.service.InsuranceCarInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 车辆信息表(InsuranceCarInfo)表控制层
@@ -16,6 +22,11 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("/insuranceCity/insuranceCarInfo")
 public class InsuranceCarInfoController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
     /**
      * 服务对象
      */
@@ -34,6 +45,15 @@ public class InsuranceCarInfoController {
 
     @PostMapping("insertOne")
     public R selectOne2(@RequestBody InsuranceCarInfo insuranceCarInfo) {
-        return R.ok().put("data",this.insuranceCarInfoService.insert(insuranceCarInfo));
+        stringRedisTemplate.opsForValue().set("insuranceCarInfo", JSON.toJSONString(insuranceCarInfo, SerializerFeature.DisableCircularReferenceDetect), 1, TimeUnit.DAYS);
+//        return R.ok().put("data",this.insuranceCarInfoService.insert(insuranceCarInfo));
+        return R.ok();
+    }
+
+    @GetMapping("getRedisCarInfo")
+    public R selectTwo(String redsiKey){
+        String s = stringRedisTemplate.opsForValue().get(redsiKey);
+        InsuranceCarInfo insuranceCarInfo = JSON.parseObject(s, InsuranceCarInfo.class);
+        return R.ok().put("data",insuranceCarInfo);
     }
 }
